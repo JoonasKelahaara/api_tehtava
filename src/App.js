bc344ac0230d9f8a75be3d0f983a1f5de5dcdef1
Joonas Kelahaara <jolle.kela@gmail.com>
2021-10-07 21:28:12 +0300

    versio yksi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const URL = 'https://api.lyrics.ovh/v1/'
+// const API_KEY = '';
+
+// Tämän API:n käyttöön ei tarvittu Api-avainta, joten jätän sen kohdan tyhjäksi,
+// sekä kommentiksi koodiin, jossa sen olisi kuulunut olla
 
 function App() {
+
+  const [artist, setArtist] = useState('');
+  const [title, setTitle] = useState('');
+  const [result, setResult] = useState('');
+  const [head, setHead] = useState('');
+
+  async function search(e) {
+    e.preventDefault();
+    try {
+      const address = URL /* + API_KEY + '/' */ + artist + '/' + title;
+      const response = await fetch(address);
+
+      if (response.ok) {
+        const json = await response.json();
+        setResult(json.lyrics);
+        setHead(artist.toUpperCase() + '-' + title.toUpperCase())
+      } else {
+        alert("Error retrieving song lyrics!"
+              + 'Make sure you wrote the artist and song name correctly!');
+      }
+    } catch(error) {
+      alert(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={search}>
+        <div>
+          <h2>Lyrics finder</h2>
+        </div>
+        <div>
+          <p>Type the artist in the artist field and the song name in the song field to find the lyrics!</p>
+        </div>
+        <div>
+          <label>Artist/band name</label>
+          <input type="text" value={artist} onChange={e => setArtist(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Song name</label>
+          <input type="text" value={title} onChange={e => setTitle(e.target.value)}></input>
+        </div>
+        <div>
+          <button>Search</button>
+          <h4>{head}</h4>
+          <p className="lyricsbox">{result}</p>
+        </div>
+      </form>
     </div>
   );
 }
